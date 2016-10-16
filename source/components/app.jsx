866e8365ca137134174fee1f0c2c9efbf91f593b
Andrew Crist <andrew.l.crist@gmail.com>
2016-10-16 13:13:50 -0600

    Capturing api array

--- source/components/app.jsx
+++ source/components/app.jsx
@@ -13,32 +13,41 @@ var Main = React.createClass({
     return(
       <div>
         <input className="user-location" placeholder="Location" onChange={(e) => this.setState({location: e.target.value})}/>
-        <button className="submit-button" type="submit" onClick={(e) => this.submit(e)}>Submit</button>
+        <button className="submit-button" type="submit" onClick={(e) => this.getWeatherLocation(e)}>Submit</button>
       </div>
     )
   },
-  submit: function(e){
-    return(
-      $.get(this.props.source + this.state.location, function(){
-        console.log(this.props.source + this.state.location);
-      }.bind(this))
-    )
+  // submit: function(e){
+  //   return(
+  //     $.get(this.props.source + this.state.location, function(){
+  //       console.log(this.props.source + this.state.location);
+  //     }.bind(this))
+  //   )
+  // },
+  getWeatherLocation: function(response){
+    var thisState = this.state.location.toLowerCase();
+    var spacelessLocation = thisState.replace(" ", '-');
+    var apiURL = 'https://weatherly-api.herokuapp.com/api/weather/' + spacelessLocation
+
+    $.get(apiURL, function(response){
+      this.setState({weather: response})
+    }.bind(this))
   }
 });
 
-var weatherDisplay = React.createClass({
-render: function(){
-    return(
-      <div className="weatherComp">
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-    )
-  }
-});
+// var weatherDisplay = React.createClass({
+// render: function(){
+//     return(
+//       <div className="weatherComp">
+//         <ul>
+//           <li></li>
+//           <li></li>
+//           <li></li>
+//           <li></li>
+//         </ul>
+//       </div>
+//     )
+//   }
+// });
 
 ReactDOM.render(<Main source='https://weatherly-api.herokuapp.com/api/weather/'/>, document.getElementById('application'));
